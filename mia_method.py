483b40d5748971fb050e1b7bbcae70793bc8a6ee
PososAgapo <bchen@kth.se>
2025-01-08 15:00:03 +0900

    test files

--- mia_method.py
+++ mia_method.py
@@ -1,7 +1,7 @@
 from torch.nn import CrossEntropyLoss
 import zlib
 import torch
-
+from torch.nn import functional as F
 class MIA:
     def __init__(self, name):
         self.name = name
@@ -90,14 +90,80 @@ class PerplexityMIA(MIA):
 class MinKMIA(MIA):
     def __init__(self):
         super().__init__("MinK")
-    def feature_compute(self, batch_logits, tokenized_inputs, target_labels, tokenizer):
-        pass
+    def feature_compute(self, batch_logits, tokenized_inputs, target_labels, tokenizer, k=0.2):
+        batch_input_ids = tokenized_inputs["input_ids"][:, 1:].unsqueeze(-1)
+        target_labels = tokenized_inputs["input_ids"].clone()
+        target_labels[tokenized_inputs["attention_mask"] == 0] = -100
+        batch_probs = F.softmax(batch_logits[:, :-1].float(), dim=-1)
+        batch_log_probs = F.log_softmax(batch_logits[:, :-1].float(), dim=-1)
+        mask = target_labels[:, 1:] != -100
+        mask = mask.unsqueeze(-1)
+        batch_token_log_probs = batch_log_probs.gather(dim=-1, index=batch_input_ids).squeeze(-1)
+        batch_probs_masked = batch_probs.where(mask, 0)
+        batch_log_probs_masked = batch_log_probs.where(mask, 0)
+        batch_mu = (batch_probs_masked.float() * batch_log_probs_masked.float()).float().sum(-1)
+        batch_sigma = ((batch_probs_masked.float() * torch.square(
+            torch.where(batch_probs_masked > 0, batch_log_probs_masked.float(),
+                        torch.tensor(0.0, device=batch_log_probs_masked.device, dtype=torch.float32)))).sum(
+            dim=-1) - torch.square(batch_mu.float()).squeeze())
+        mask = mask.squeeze(-1)
+        batch_mink_plus = (batch_token_log_probs - batch_mu).float() * mask / batch_sigma.float().sqrt()
+        token_length = mask.sum(dim=1)
+        batch_mink_plus[mask == False] = torch.inf
+        batch_token_log_probs[mask == False] = torch.inf
+        sorted_mink_plus, _ = torch.sort(batch_mink_plus)
+        sorted_mink, _ = torch.sort(batch_token_log_probs)
+        batch_mink_plus_avg = []
+        batch_mink_avg = []
+        for i, length in enumerate(token_length):
+            caculate_length = int(length * 0.2) if length > 5 else length
+            front_values = sorted_mink_plus[i, :caculate_length]
+            avg = torch.mean(front_values.float()).item()
+            batch_mink_plus_avg.append(avg)
+            front_values = sorted_mink[i, :caculate_length]
+            avg = torch.mean(front_values.float()).item()
+            batch_mink_avg.append(avg)
+        return batch_mink_avg
+
 
 class MinKPlusMIA(MIA):
     def __init__(self):
         super().__init__("MinKPlus")
-    def feature_compute(self, batch_logits, tokenized_inputs, target_labels, tokenizer):
-        pass
+
+    def feature_compute(self, batch_logits, tokenized_inputs, target_labels, tokenizer, k=0.2):
+        batch_input_ids = tokenized_inputs["input_ids"][:, 1:].unsqueeze(-1)
+        target_labels = tokenized_inputs["input_ids"].clone()
+        target_labels[tokenized_inputs["attention_mask"] == 0] = -100
+        batch_probs = F.softmax(batch_logits[:, :-1].float(), dim=-1)
+        batch_log_probs = F.log_softmax(batch_logits[:, :-1].float(), dim=-1)
+        mask = target_labels[:, 1:] != -100
+        mask = mask.unsqueeze(-1)
+        batch_token_log_probs = batch_log_probs.gather(dim=-1, index=batch_input_ids).squeeze(-1)
+        batch_probs_masked = batch_probs.where(mask, 0)
+        batch_log_probs_masked = batch_log_probs.where(mask, 0)
+        batch_mu = (batch_probs_masked.float() * batch_log_probs_masked.float()).float().sum(-1)
+        batch_sigma = ((batch_probs_masked.float() * torch.square(
+            torch.where(batch_probs_masked > 0, batch_log_probs_masked.float(),
+                        torch.tensor(0.0, device=batch_log_probs_masked.device, dtype=torch.float32)))).sum(
+            dim=-1) - torch.square(batch_mu.float()).squeeze())
+        mask = mask.squeeze(-1)
+        batch_mink_plus = (batch_token_log_probs - batch_mu).float() * mask / batch_sigma.float().sqrt()
+        token_length = mask.sum(dim=1)
+        batch_mink_plus[mask == False] = torch.inf
+        batch_token_log_probs[mask == False] = torch.inf
+        sorted_mink_plus, _ = torch.sort(batch_mink_plus)
+        sorted_mink, _ = torch.sort(batch_token_log_probs)
+        batch_mink_plus_avg = []
+        batch_mink_avg = []
+        for i, length in enumerate(token_length):
+            caculate_length = int(length * 0.2) if length > 5 else length
+            front_values = sorted_mink_plus[i, :caculate_length]
+            avg = torch.mean(front_values.float()).item()
+            batch_mink_plus_avg.append(avg)
+            front_values = sorted_mink[i, :caculate_length]
+            avg = torch.mean(front_values.float()).item()
+            batch_mink_avg.append(avg)
+        return batch_mink_plus_avg
 
 class RecallMIA(MIA):
     def __init__(self):
