d32dd4e01d44f9fd5d82152bf7a2b5dd00dbf724
PososAgapo <bchen@kth.se>
2025-01-13 16:01:18 +0900

    test eda pac

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ from mia_method import MinKPlusMIA
 from mia_method import SaMIA
 from mia_method import CDDMIA
 from mia_method import ReferenceMIA
+from mia_method import EDAPACMIA
 
 test_data = WikiMIA("WikiMIA", length=64)
 model = GPTNeoX("70m")
-refer_method = ReferenceMIA()
-member_feature_value_dict = model.collect_outputs(test_data.member, refer_method)
-non_member_feature_value_dict = model.collect_outputs(test_data.non_member, refer_method)
+eda_pac_method = EDAPACMIA()
+member_feature_value_dict = model.collect_outputs(test_data.member, eda_pac_method)
+non_member_feature_value_dict = model.collect_outputs(test_data.non_member, eda_pac_method)
 
 
 
