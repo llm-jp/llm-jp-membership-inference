86ad6e15f3de0c13c266a95c0c6a1bed83c2b916
PososAgapo <bchen@kth.se>
2025-01-13 13:48:43 +0900

    test refer model

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from mia_method import ReferenceMIA
 
 test_data = WikiMIA("WikiMIA", length=64)
 model = GPTNeoX("70m")
-refer_method = ReferenceMIA("Refer")
+refer_method = ReferenceMIA()
 member_feature_value_dict = model.collect_outputs(test_data.member, refer_method)
 non_member_feature_value_dict = model.collect_outputs(test_data.non_member, refer_method)
 
