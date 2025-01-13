f1570119c905ee1406b32952f07164862d88862c
PososAgapo <bchen@kth.se>
2025-01-13 13:34:03 +0900

    test refer model

--- test.py
+++ test.py
@@ -5,17 +5,13 @@ from mia_method import MinKMIA
 from mia_method import MinKPlusMIA
 from mia_method import SaMIA
 from mia_method import CDDMIA
+from mia_method import ReferenceMIA
 
 test_data = WikiMIA("WikiMIA", length=64)
 model = GPTNeoX("70m")
-mia_method = LossMIA()
-mink_method = MinKMIA()
-mink_plus_method = MinKPlusMIA()
-samia_method = SaMIA()
-cdd_method  = CDDMIA()
-
-member_feature_value_dict = model.collect_outputs(test_data.member, cdd_method)
-non_member_feature_value_dict = model.collect_outputs(test_data.non_member, cdd_method)
+refer_method = ReferenceMIA("Refer")
+member_feature_value_dict = model.collect_outputs(test_data.member, refer_method)
+non_member_feature_value_dict = model.collect_outputs(test_data.non_member, refer_method)
 
 
 
