ed7d19d488e06f47987797522a62c24a196cf2cd
PososAgapo <bchen@kth.se>
2025-01-15 15:51:21 +0900

    add some dataset

--- mia_dataset.py
+++ mia_dataset.py
@@ -37,9 +37,44 @@ class WikiMIA(MIADataset):
         self.member = TextDataset(self.member)
         self.non_member = TextDataset(self.non_member)
 
-class TemporalArixivMIA(MIADataset):
+class TemporalArxiv(MIADataset):
     def __init__(self):
         super().__init__("TemporalArixivMIA")
         self._init_dataset()
     def _init_dataset(self):
-        pass
+        self.dataset = load_dataset("iamgroot42/mimir", "temporal_arxiv")
+        self.member = TextDataset(self.dataset["member"])
+        self.non_member = TextDataset(self.dataset["non_member"])
+
+class TemporalWiki(MIADataset):
+    def __init__(self):
+        super().__init__("TemporalWikiMIA")
+        self._init_dataset()
+    def _init_dataset(self):
+        self.dataset = load_dataset("iamgroot42/mimir", "temporal_wiki")
+        self.member = TextDataset(self.dataset["member"])
+        self.non_member = TextDataset(self.dataset["non_member"])
+
+class MIMIR(MIADataset):
+    def __init__(self, domain="all"):
+        super().__init__("MIMIR")
+        self._init_dataset()
+        self.domain = domain
+    def _init_dataset(self):
+        if self.domain == "all":
+            self.dataset = {}
+            for domain in ["arxiv", "dm_mathematics", "github", "hackernews", "pile_cc",
+                           "pubmed_central", "wikipedia_(en)", "full_pile", "c4"]:
+                self.dataset[domain] = load_dataset("iamgroot42/mimir", domain, split = "ngram_13_0.8")
+                self.member = {}
+                self.non_member = {}
+                for domain in self.dataset:
+                    self.member[domain] = TextDataset(self.dataset[domain]["member"])
+                    self.non_member[domain] = TextDataset(self.dataset[domain]["non_member"])
+        else:
+            self.dataset = {}
+            self.dataset[self.domain] = load_dataset("iamgroot42/mimir", self.domain, split="ngram_13_0.8" )
+            self.member = {}
+            self.member[self.domain] = TextDataset(self.dataset[self.domain]["member"])
+            self.non_member = {}
+            self.non_member[self.domain] = TextDataset(self.dataset[self.domain]["non_member"])
